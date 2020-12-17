1af3c78c9c4b720b0c39b8c45b980ad40d2fb2f5
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-12-17 16:17:27 +0100

    json

--- src/Trill.Api/Startup.cs
+++ src/Trill.Api/Startup.cs
@@ -1,18 +1,27 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Trill.Application;
+using Trill.Application.Commands;
 using Trill.Application.Services;
 using Trill.Infrastructure;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Trill.Api
 {
     public class Startup
     {
+        private readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
         private readonly IConfiguration _configuration;
 
         public Startup(IConfiguration configuration)
@@ -57,29 +66,32 @@ namespace Trill.Api
                 {
                     var storyId = Guid.Parse(context.Request.RouteValues["storyId"].ToString());
                     var storyService = context.RequestServices.GetRequiredService<IStoryService>();
-                    if (storyId == Guid.Empty)
+                    var story = await storyService.GetAsync(storyId);
+                    if (story is null)
                     {
                         context.Response.StatusCode = 404;
                         return;
                     }
-                    
+
                     context.Response.ContentType = "application/json";
-                    await context.Response.WriteAsync("{}");
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(story, _jsonSerializerSettings));
                 });
 
                 endpoints.MapGet("stories", async context =>
                 {
                     var storyService = context.RequestServices.GetRequiredService<IStoryService>();
+                    var stories = await storyService.BrowseAsync();
                     context.Response.ContentType = "application/json";
-                    await context.Response.WriteAsync("[]");
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(stories, _jsonSerializerSettings));
                 });
                 
                 endpoints.MapPost("stories", async context =>
                 {
+                    var json = await new StreamReader(context.Request.Body).ReadToEndAsync();
+                    var command = JsonConvert.DeserializeObject<SendStory>(json);
                     var storyService = context.RequestServices.GetRequiredService<IStoryService>();
-                    var storyId = Guid.NewGuid();
-                    // Call application layer and save data...
-                    context.Response.Headers.Add("Location", $"stories/{storyId}");
+                    await storyService.AddAsync(command);
+                    context.Response.Headers.Add("Location", $"stories/{command.Id}");
                     context.Response.StatusCode = 201;
                 });
             });
