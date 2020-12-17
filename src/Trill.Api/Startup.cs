3c8b50fbeaac342729b6fb5a81ab178cc42db6db
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-12-17 12:14:35 +0100

    options

--- src/Trill.Api/Startup.cs
+++ src/Trill.Api/Startup.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Trill.Application.Services;
+using Trill.Infrastructure;
 
 namespace Trill.Api
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
         // Configure IoC container
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IMessenger, Messenger>();
             services.AddSingleton<IMessenger, MessengerV2>();
+            services.Configure<ApiOptions>(_configuration.GetSection("api"));
         }
 
         // Configure middleware
@@ -31,6 +42,7 @@ namespace Trill.Api
             {
                 endpoints.MapGet("", context =>
                 {
+                    var options = context.RequestServices.GetRequiredService<IOptions<ApiOptions>>();
                     var messenger = context.RequestServices.GetRequiredService<IMessenger>();
                     return context.Response.WriteAsync("Hello world!");
                 });
---
+++ src/Trill.Infrastructure/ApiOptions.cs
@@ -0,0 +1,7 @@
+namespace Trill.Infrastructure
+{
+    public class ApiOptions
+    {
+        public string Name { get; set; }
+    }
+}
