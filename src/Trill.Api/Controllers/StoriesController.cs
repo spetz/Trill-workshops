12a5adc6bab683abccc27ac31af6e065de4e98ed
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-12-18 10:14:44 +0100

    controllers

---
+++ src/Trill.Api/Controllers/StoriesController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Trill.Application.Commands;
+using Trill.Application.DTO;
+using Trill.Application.Services;
+
+namespace Trill.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StoriesController : ControllerBase
+    {
+        private readonly IStoryService _storyService;
+
+        public StoriesController(IStoryService storyService)
+        {
+            _storyService = storyService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StoryDto>>> Get(string author)
+            => Ok(await _storyService.BrowseAsync(author));
+    
+        [HttpGet("{storyId:guid}")]
+        public async Task<ActionResult<StoryDto>> Get(Guid storyId)
+        {
+            var story = await _storyService.GetAsync(storyId);
+            if (story is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(story);
+        }
+    
+        [HttpPost]
+        public async Task<ActionResult> Post(SendStory command)
+        {
+            await _storyService.AddAsync(command);
+
+            return CreatedAtAction(nameof(Get), new {storyId = command.Id}, null);
+        }
+    }
+}
